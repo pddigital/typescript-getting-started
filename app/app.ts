b569410170a6d73fbb9a0a7772d26888f0d31a75
Paul Day <pday@ordermygear.com>
2020-03-28 16:45:11 -0500

    initial commit

---
+++ app/app.ts
@@ -0,0 +1,7 @@
+function startGame() {
+    // starting a new game
+    const messagesElement = document.getElementById('messages');
+    messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';  
+  }
+  
+  document.getElementById('startGame')!.addEventListener('click', startGame);
